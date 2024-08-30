e4623769c0b8381518c01b377812e1c89dc394cf
Khojiakbar1111 <kohinoorroshan81@gmail.com>
2024-08-30 23:28:33 +0500

    components have been added to App.js

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,33 +1,31 @@
-import './Header.css'
-import logo from '../../assets/logo.png'
-import {  NavLink } from 'react-router-dom'
-
+import "./Header.css";
+import logo from "../../assets/logo.png";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
-    return (
-        <header>
+  return (
+    <header>
+      <a href="#" className="logo">
+        <img src={logo} alt="logo" />
+        <h4>Notitanic</h4>
+      </a>
 
-            <a href="#" className='logo'>
-                <img src={logo} alt="logo" />
-                <h4>Notitanic</h4>
-            </a>
+      <ul>
+        <li>
+          <NavLink to="/home">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/aboutme">About me</NavLink>
+        </li>
+        <li>
+          <NavLink to="/portfolio">Portfolio</NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact">Contact</NavLink>
+        </li>
+      </ul>
+    </header>
+  );
+};
 
-            <ul>
-                <li>
-                    <NavLink to='/home'>Home</NavLink>
-                </li>
-                <li>
-                    <NavLink to='/aboutme'>About me</NavLink>
-                </li>
-                <li>
-                    <NavLink to='/portfolio'>Portfolio</NavLink>
-                </li>
-                <li>
-                    <NavLink to='/contact'>Contact</NavLink>
-                </li>
-            </ul>
-        </header>
-    )
-}
-
-export default Header
+export default Header;
