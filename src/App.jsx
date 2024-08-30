865cf59fb7c45e7efceab6852c0bd8043b5fac5d
ozodbekMirsaidov <mirsaidovozodbek28@gmail.com>
2024-08-30 16:01:20 +0500

    lorem

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import './App.css'
   return (
     <div className="App">
       <h1>Hello</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore veniam itaque eum quos ipsum quod. Perspiciatis ratione voluptate earum esse!</p>
+      <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, est.</span>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta animi quam, labore corporis ab non minus recusandae facilis temporibus saepe.</p>
     </div>
   )
 
